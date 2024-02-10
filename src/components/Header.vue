f3496e01e8b28b487d7091c4a8c83cb86c40f421
claudiainbytes <claudiaeforero@hotmail.com>
2024-02-09 23:06:07 -0500

    Creating subcomponents nested in App

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="col-span-12 lg:col-span-4 p-4">
+        <div class="w-full h-full flex flex-col justify-start">
+          <div class="w-full text-center p-4 pb-0 mb-4">
+            <div class="avatar">
+              <div class="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src="https://claudiainbytes.github.io/img/about.jpg" />
+              </div>
+            </div>
+          </div>
+          <div class="w-full text-center p-4 mb-0 prose max-w-none">
+            <h1 class="my-0 text-primary">Claudia Estupiñán</h1>
+            <h3 class="my-0">Front-end Web Developer</h3>
+          </div>
+          <div class="w-full text-center p-4 prose prose-a:text-lg max-w-none">
+            <h4 class="my-0"><i class="fa-solid fa-location-dot text-lg mr-2 mb-2"></i>Bogotá, Colombia</h4>
+            <a class="social-media-icon-link"><i class="fa-regular fa-envelope"></i></a>
+            <a class="social-media-icon-link"><i class="fa-brands fa-linkedin"></i></a>
+            <a class="social-media-icon-link"><i class="fa-brands fa-github"></i></a>
+            <a class="social-media-icon-link"><i class="fa-brands fa-twitter"></i></a>
+          </div>
+        </div>  
+    </header>
+</template>
+<script setup>
+</script>
