da1df8a7c7e94707bac7990cdf12345c56e21f20
claudiainbytes <claudiaeforero@hotmail.com>
2024-03-12 23:43:34 -0500

    Adjust routes

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const router = createRouter({
       {
         path: '/:catchAll(.*)',
         name: '404Name',
-        component: Intro
+        redirect: { name: 'Intro' } 
       }
     ]
   })
