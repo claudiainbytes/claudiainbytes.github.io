83f63b268de86ac6ea9a61267601bcc61e1dafe0
claudiainbytes <claudiaeforero@hotmail.com>
2024-03-12 21:48:58 -0500

    Adjust Vite and Package JSON

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/claudiainbytes2024/',
+  base: '/',
   plugins: [vue()],
   server: {
     port: 3000,
